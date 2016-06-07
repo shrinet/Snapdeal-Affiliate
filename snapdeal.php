7896cd0c63841bc6e6b424d5aeb130e887959b44
Akhilesh Singh Shrinet <alpha007_2008@yahoo.com>
2016-06-07 11:19:29 +0530

    Initial Uploads

---
+++ snapdeal.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+Plugin Name: Snapdeal Affiliate
+Plugin URI: https://www.maxvisionsolutions.com
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: Akhilesh Singh Shrinet
+Author URI: https://www.maxvisionsolutions.com
+License: GPL2
+*/
+class Wp_Snapdeal_Affiliate{
+	// Constructor
+	function __construct() {
+
+		add_action( 'admin_menu', array( $this, 'wpsa_add_menu' ));
+		add_action( 'admin_init', 'wpsa_plugin_settings' );
+		register_activation_hook( __FILE__, array( $this, 'wpsa_install' ) );
+		register_deactivation_hook( __FILE__, array( $this, 'wpsa_uninstall' ) );
+	}
+
+	function wpsa_add_menu() {
+		add_menu_page('My Plugin Settings', 'Plugin Settings', 'administrator', 'my-plugin-settings', 'my_plugin_settings_page', 'dashicons-admin-generic');
+	}
+
+
+
+	function wpsa_plugin_settings() {
+		register_setting( 'wpsa-settings-group', 'accountant_name' );
+		register_setting( 'wpsa-settings-group', 'affiliate_id' );
+		register_setting( 'wpsa-settings-group', 'affiliate_token' );
+	}
+
+
+}
+new Wp_Snapdeal_Affiliate();
---
+++ uninstall.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Akhilesh Singh
+ * Date: 6/2/2016
+ * Time: 5:15 PM
+ */
